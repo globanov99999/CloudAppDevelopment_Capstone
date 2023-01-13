b3eaee4fcbffe90c605f67eb2961ee769fb32d0c
globanov99999 <globanov99999@gmail.com>
2023-01-13 14:41:10 +0300

    contarization1

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+if [ "$DATABASE" = "postgres" ]; then
+    echo "Waiting for postgres database ..."
+    while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
+    sleep 0.1
+    done
+    echo "Database PostgreSQL started"
+fi
+
+echo "Preparing migrations and  updating the database. "
+python manage.py makemigrations main --noinput
+python manage.py migrate --noinput
+exec "$@"
